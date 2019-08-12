b9d629b41b1fd7273409c6e64cdce07c2b970d78
Ibrahim Samad <naatogma@gmail.com>
2019-08-12 08:29:31 +0000

    Refactor mutations with mutation-types

--- src/store/index.js
+++ src/store/index.js
@@ -3,7 +3,7 @@ import Vuex from 'vuex'
 
 import state from './state'
 import * as getters from './getters'
-import * as mutations from './mutations'
+import mutations from './mutations'
 import * as actions from './actions'
 
 Vue.use(Vuex)
--- src/store/mutations.js
+++ src/store/mutations.js
@@ -1,7 +1,17 @@
 
-export const ADD_TODO = (state, item) => state.todos.push(item)
-export const REMOVE_TODO = (state, todo) => state.todos = state.todos.filter(item => item.id != todo.id)
-export const UPDATE_TODO = (state, todo) => {
-    const index = state.todos.findIndex((item) => item.id == todo.id)
-    state.todos.splice(index, 1, todo)
+import {ADD_TODO, REMOVE_TODO, UPDATE_TODO} from './mutation-types';
+
+export default {
+    [ADD_TODO] (state, item) {
+        state.todos.push(item)
+    },
+
+    [REMOVE_TODO](state, todo) {
+        state.todos = state.todos.filter(item => item.id != todo.id)
+    },
+
+    [UPDATE_TODO](state, todo) {
+        const index = state.todos.findIndex((item) => item.id == todo.id)
+        state.todos.splice(index, 1, todo)
+    }
 }
